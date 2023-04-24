c2c2e6b0dcefa4d631a5a7d2c815be7a5eb676b4
JEL666 <dlawodms12@gmail.com>
2023-04-24 22:49:48 +0900

    방 컨셉 설정 기능 추가

--- src/pages/CreateRoom.js
+++ src/pages/CreateRoom.js
@@ -1,13 +1,13 @@
 import styled from "@emotion/styled";
-import Button from '@mui/material/Button';
-import { createTheme } from '@mui/material/styles';
+import Button from "@mui/material/Button";
+import { createTheme } from "@mui/material/styles";
 import axios from "axios";
 import { Spacer, Spinner } from "../components";
 import useForm from "../hooks/useForm";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
-import { Title2 } from "../components/domain";
+import { ImageRadioGroup, Title2 } from "../components/domain";
 
 const PageWrapper = styled.div`
 `;
@@ -25,7 +25,7 @@ const FormBox = styled.form`
     box-shadow: 6.79014px 6.79014px 20.3704px 5.43212px rgba(0, 0, 0, 0.14);
 `;
 
-const InputContainer = styled.div`
+const InputWrapper = styled.div`
     margin-top: 30px;
 `;
 
@@ -89,11 +89,16 @@ const StyledSubTitle = styled.div`
     margin: 35px;
 `;
 
+const ListDisplayer = styled.div`
+    display: flex;
+
+`;
+
 const ButtonTheme = createTheme({
   palette: {
     primary: {
       // light: will be calculated from palette.primary.main,
-      main: '#ffcdd2',
+      main: "#ffcdd2",
     }
   }
 });
@@ -104,6 +109,12 @@ export default function CreateRoom() {
     const [errorMessage, setErrorMessage] = useState("");
     const [minDate, setMinDate] = useState(); 
     const [maxDate, setMaxDate] = useState(); 
+    const [page, setPage] = useState(0);
+
+    const [backgroundStyle, setBackgroundStyle] = useState(null);
+    const [messageStyle, setMessageStyle] = useState(null);
+    const [presentStyle, setPresentStyle] = useState(null);
+
     useEffect(() => {
         const maxDate = new Date();
         maxDate.setFullYear(maxDate.getFullYear() + 1);
@@ -115,15 +126,18 @@ export default function CreateRoom() {
 
     }, []);
 
-    const { isLoading, errors, handleChange, handleSubmit } = useForm({
+    const { values, isLoading, errors, handleChange, handleSubmit } = useForm({
         initialState: {
-            roomName: '',
-            roomBirthdate: '',
-            roomEmail: ''
+            roomName: "",
+            roomBirthdate: "",
+            roomEmail: "",
+            background: "",
+            presnet: "",
+            message: ""
         },
         onSubmit: async ({ roomName, roomBirthdate, roomEmail }) => {
             try {
-                const res = await axios.post('/new-room', null, {
+                const res = await axios.post("/new-room", null, {
                     params: {
                         roomName,
                         roomBirthdate,
@@ -131,7 +145,7 @@ export default function CreateRoom() {
                         roomCategory: "BIRTHDAY"
                     }
                 });
-                console.log(res);
+
                 if (res.status === 200) {
                     navigate(`/gift/${res.data}`);
                 }
@@ -143,13 +157,50 @@ export default function CreateRoom() {
         },
         validate: ({ roomName, roomEmail, roomBirthdate }) => {
             const errors = {};
-            if (!/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(roomEmail)) errors.roomEmail = '올바른 이메일을 입력해주세요.'; // @o.cnu.ac.kr 같은 이메일은 통과가 안되니 수정필요
-            if (!roomName) errors.roomName = '방 이름을 입력해주세요.';
-            if (!roomBirthdate) errors.roomBirthdate = '생일을 입력해주세요.';
+            if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(roomEmail)) errors.roomEmail = "이메일 형식이 올바르지 않습니다."; // @o.cnu.ac.kr 같은 이메일은 통과가 안되니 수정필요
+            if (!roomName) errors.roomName = "방 이름을 입력해주세요.";
+            if (!roomBirthdate) errors.roomBirthdate = "생일을 입력해주세요.";
             return errors;
         }
     });
 
+    const handleCheckPage0 = () => {
+        setErrorMessage({});
+        const errors = {};
+
+        if (!values["roomName"]) errors.roomName = "방 이름을 입력해주세요.";
+        if (!values["roomBirthdate"]) errors.roomBirthdate = "생일을 입력해주세요.";
+        if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(values["roomEmail"])) errors.roomEmail = "이메일 형식이 올바르지 않습니다.";
+
+        if (Object.keys(errors).length === 0) {
+            setPage(1);
+        }
+        setErrorMessage(errors);
+    }
+
+    const handleCheckPage1 = () => {
+        setErrorMessage({});
+        const errors = {};
+
+        if (!backgroundStyle) errors.backgroundStyle = "배경을 선택해주세요.";
+        if (!messageStyle) errors.messageStyle = "메시지을 선택해주세요.";
+        if (!presentStyle) errors.presentStyle = "선물을 선택해주세요.";
+        
+        if (Object.keys(errors).length === 0) {
+            setPage(2);
+        }
+        setErrorMessage(errors);
+    }
+
+    const handleChangeBackground = (e) => {
+        setBackgroundStyle(e.target.value);
+    }
+    const handleChangeMessage = (e) => {
+        setMessageStyle(e.target.value);
+    }
+    const handleChangePresent = (e) => {
+        setPresentStyle(e.target.value);
+    }
     return (
         <PageWrapper>
             <TitleWrapper>
@@ -157,47 +208,131 @@ export default function CreateRoom() {
             </TitleWrapper>
             <FormBox onSubmit={handleSubmit}>
                 <Spacer type="vertical">
-                    <StyledTitle>새로운 방 생성하기</StyledTitle>
-                    <InputContainer>
-                        <StyledSubTitle>방 이름</StyledSubTitle>
-                        <Input
-                        name="roomName"
-                        type="text"
-                        placeholder="방 이름을 입력해주세요."
-                        onChange={handleChange}
-                        maxlength = '10'
-                        style={{width:200, height:30, fontSize: 20}}
-                        />
-                        <ErrorMessage>{errors.roomName}</ErrorMessage>
-                    </InputContainer>
-                    <InputContainer>
-                        <StyledSubTitle>축하받을 날을 지정해주세요.</StyledSubTitle>
-                        <StyledInput
-                        name="roomBirthdate"
-                        type="date"
-                        min={minDate}
-                        max={maxDate}
-                        onChange={handleChange}
-                        />
-                        <ErrorMessage>{errors.roomBirthdate}</ErrorMessage>
-                    </InputContainer>
-                    <InputContainer>
-                        <StyledSubTitle>이메일을 입력해주세요. </StyledSubTitle>
-                        <Input
-                        name="roomEmail"
-                        type="email"
-                        label="선물을 받는 사람의 이메일을 입력해주세요."
-                        placeholder="선물을 받는 사람의 이메일을 입력해주세요."
-                        onChange={handleChange}
-                        style={{width:400, height:30, fontSize: 20}}
-                        />
-                        <ErrorMessage>{errors.roomEmail}{errorMessage}</ErrorMessage>
-                    </InputContainer>
-                    <InputContainer>
-                        <Button variant="contained" type="submit" theme={ButtonTheme} sx={{ width: 200, padding: 1, margin: 2, fontSize: 20}} disabled={isLoading} > {isLoading ? <Spinner /> : '링크 생성하기' }</Button>
-                    </InputContainer>
+                    <div style={{ display: page === 0 ? "block" : "none" }}>
+                        <StyledTitle>새로운 방 생성하기</StyledTitle>
+                        <InputWrapper>
+                            <StyledSubTitle>방 이름</StyledSubTitle>
+                            <Input
+                                name="roomName"
+                                type="text"
+                                placeholder="방 이름을 입력해주세요."
+                                onChange={handleChange}
+                                maxlength = "10"
+                                style={{width:200, height:30, fontSize: 20}}
+                            />
+                            <ErrorMessage>{errorMessage.roomName}</ErrorMessage>
+                        </InputWrapper>
+                        <InputWrapper>
+                            <StyledSubTitle>축하받을 날을 지정해주세요.</StyledSubTitle>
+                            <StyledInput
+                                name="roomBirthdate"
+                                type="date"
+                                min={minDate}
+                                max={maxDate}
+                                onChange={handleChange}
+                            />
+                            <ErrorMessage>{errorMessage.roomBirthdate}</ErrorMessage>
+                        </InputWrapper>
+                        <InputWrapper>
+                            <StyledSubTitle>이메일을 입력해주세요. </StyledSubTitle>
+                            <Input
+                                name="roomEmail"
+                                type="email"
+                                label="선물을 받는 사람의 이메일을 입력해주세요."
+                                placeholder="선물을 받는 사람의 이메일을 입력해주세요."
+                                onChange={handleChange}
+                                style={{width:400, height:30, fontSize: 20}}
+                            />
+                            <ErrorMessage>{errorMessage.roomEmail}</ErrorMessage>
+                        </InputWrapper>
+                        <InputWrapper>
+                            <Button variant="contained" type="button" theme={ButtonTheme} sx={{ width: 200, padding: 1, margin: 2, fontSize: 20}} disabled={isLoading} onClick={handleCheckPage0}>다음으로</Button>
+                        </InputWrapper>
+                    </div>
+                    <div style={{ display: page === 1 ? "block" : "none" }}>
+                        <StyledTitle>방 디자인 선택하기</StyledTitle>
+                        <InputWrapper>
+                            <StyledSubTitle>배경 선택</StyledSubTitle>
+                            <ImageRadioGroup list={backgroundList} name="background" onChange={handleChangeBackground} />
+                            <ErrorMessage>{errorMessage.backgroundStyle}</ErrorMessage>
+                        </InputWrapper>
+                        <InputWrapper>
+                            <StyledSubTitle>메시지 선택</StyledSubTitle>
+                            <ImageRadioGroup list={messageList} name="message" onChange={handleChangeMessage}/>
+                            <ErrorMessage>{errorMessage.messageStyle}</ErrorMessage>
+                        </InputWrapper>
+                        <InputWrapper>
+                            <StyledSubTitle>선물 선택</StyledSubTitle>
+                            <ImageRadioGroup list={presentList} name="present" onChange={handleChangePresent} />
+                            <ErrorMessage>{errorMessage.presentStyle}</ErrorMessage>
+                        </InputWrapper>
+                        <InputWrapper>
+                            <Button variant="contained" type="button" theme={ButtonTheme} sx={{ width: 200, padding: 1, margin: 2, fontSize: 20}} disabled={isLoading} onClick={handleCheckPage1}>다음으로</Button>
+                        </InputWrapper>
+                    </div>
+                    <div style={{ display: page === 2 ? "block" : "none" }}>
+                        <StyledTitle>미리보기</StyledTitle>
+                        <div></div>
+                        <Button variant="contained" type="submit" theme={ButtonTheme} sx={{ width: 200, padding: 1, margin: 2, fontSize: 20}} disabled={isLoading} onClick={handleCheckPage1}> {isLoading ? <Spinner /> : "방 생성하기" }</Button>
+                    </div>
                 </Spacer>
             </FormBox>
         </PageWrapper>
     );
 }
+
+const PUBLIC_URL = process.env.PUBLIC_URL;
+
+const backgroundList = [
+    {
+        src: PUBLIC_URL + "/contents-design-birthday/basiccake.png",
+        alt: "birthday",
+        value: "birthday"
+    },
+    {
+        src: PUBLIC_URL + "/contents-design-birthday/candle.png",
+        alt: "candle",
+        value: "candle"
+    },
+    {
+        src: PUBLIC_URL + "/contents-design-birthday/label.png",
+        alt: "label",
+        value: "label"
+    },
+];
+
+const messageList = [
+    {
+        src: PUBLIC_URL + "/contents-design-birthday/basiccake.png",
+        alt: "birthday",
+        value: "birthday"
+    },
+    {
+        src: PUBLIC_URL + "/contents-design-birthday/candle.png",
+        alt: "candle",
+        value: "candle"
+    },
+    {
+        src: PUBLIC_URL + "/contents-design-birthday/label.png",
+        alt: "label",
+        value: "label"
+    },
+];
+
+const presentList = [
+    {
+        src: PUBLIC_URL + "/contents-design-birthday/basiccake.png",
+        alt: "birthday",
+        value: "birthday"
+    },
+    {
+        src: PUBLIC_URL + "/contents-design-birthday/candle.png",
+        alt: "candle",
+        value: "candle"
+    },
+    {
+        src: PUBLIC_URL + "/contents-design-birthday/label.png",
+        alt: "label",
+        value: "label"
+    },
+];
